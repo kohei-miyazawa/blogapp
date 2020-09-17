9ad2cabc7d8f1bec86b318a9a5f7c253e8266a90
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-09-17 16:43:59 +0900

    Ajax実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,16 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
 
+  def show
+    article = Article.find(params[:article_id])
+    like_status = current_user.has_liked?(article)
+    render json: { hasLiked: like_status }
+  end
+
   def create
     article = Article.find(params[:article_id])
     article.likes.create!(user_id: current_user.id)
-    redirect_to article_path(article)
+    render json: { status: 'ok' }
   end
 
   def destroy
@@ -12,6 +18,6 @@ class LikesController < ApplicationController
     like = article.likes.find_by!(user_id: current_user.id)
 
     like.destroy!
-    redirect_to article_path(article)
+    render json: { status: 'ok' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resource :timeline, only: [:show]
 
   resources :articles do
-    resources :comments, only: [:new, :create]
-    resource :like, only: [:create, :destroy]
+    resources :comments, only: [:new, :create, :index]
+    resource :like, only: [:show, :create, :destroy]
   end
 
   resources :accounts, only: [:show] do
